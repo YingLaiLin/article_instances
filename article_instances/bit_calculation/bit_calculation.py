e7aef5e12a54bcefc08ccf854839b7cb60df0b62
leon <18850544602@163.com>
2018-01-12 18:58:03 +0800

    更新文章 '位运算的妙用' 中的两个代码

--- article_instances/bit_calculation/bit_calculation.py
+++ article_instances/bit_calculation/bit_calculation.py
@@ -1,6 +1,8 @@
 def main():
     # res = count_1s_basically(26)
-    res = count_1s_enhanced(26)
+    # res = count_1s_enhanced(26)
+    # res = count_1s_simultaneously(26)
+    res = count_1s_simultaneously_enhanced(26)
     print(res)
 
 
@@ -19,6 +21,26 @@ def count_1s_enhanced(x):
         count += 1
         x = x & (x - 1)
     return count
+
+
+def count_1s_simultaneously(x):
+    count = 0
+    operators = [0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff]
+    blocks = [x & operator for operator in operators]  # 列表推导
+    for block in blocks:
+        count += count_1s_enhanced(block)
+    return count
+
+
+def count_1s_simultaneously_enhanced(x):
+    val = 0
+    for index in range(8):
+        val += x & 0x0101010101010101
+        x >>= 1
+    val += val >> 32
+    val += val >> 16
+    val += val >> 8
+    return val & 0xff
 
 
 if __name__ == "__main__":
